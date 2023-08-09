99d30fa6676b5e97409a3c036a7419173327160e
Brandon Teinert <10935840+bteinert@users.noreply.github.com>
2023-08-09 14:36:23 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
-const { withFaust, getWpHostname } = require('@faustwp/core');
-
-/**
- * @type {import('next').NextConfig}
- **/
-module.exports = withFaust({
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['node_modules'],
-  },
-  images: {
-    domains: [getWpHostname()],
-  },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-  });
 module.exports = () => {
   const rewrites = () => {
     return [
@@ -49,4 +31,26 @@ module.exports = () => {
     ]
   }, **/ 
 };
+
+
+
+const { withFaust, getWpHostname } = require('@faustwp/core');
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = withFaust({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['node_modules'],
+  },
+  images: {
+    domains: [getWpHostname()],
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  });
+
 
