4c1ddabf63e26cd37e7e9cb2233b9af34ab524a4
Brandon Teinert <10935840+bteinert@users.noreply.github.com>
2023-08-09 17:13:47 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,23 +1,21 @@
-import { NextResponse } from 'next/server'
- 
-export function middleware(request) {
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-  const hostname = 'brandonteinert.wpengine.com' // or 'eu.posthog.com'
-
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('host', hostname)
-
-  let url = request.nextUrl.clone()
-  url.protocol = 'http'
-  url.hostname = hostname
-  url.port = 80
-  url.pathname = url.pathname.replace(/^\/sample/, 'sample-page/');
-
-  return NextResponse.rewrite(url, {
-    headers: requestHeaders,
-  })
-}
- 
 export const config = {
-  matcher: '/sample/:path*',
+  matcher: '/sample/',
+};
+
+// Proxies /sample/ to http://brandonteinert.wpengine.com/sample-page
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('brandonteinert.wpengine.com', 'brandonteinert.wpengine.com');
+
+  // Extract product id from pathname
+  request.nextUrl.href = `http://brandonteinert.wpengine.com/sample-page/`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
