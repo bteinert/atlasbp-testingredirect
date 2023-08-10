3eb6f1f22a9adebf3a2f02ae5c2eb4c94107050f
Brandon Teinert <10935840+bteinert@users.noreply.github.com>
2023-08-10 15:46:28 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,36 +3,15 @@ const { withFaust, getWpHostname } = require('@faustwp/core');
  * @type {import('next').NextConfig}
  **/
 module.exports = withFaust({
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['node_modules'],
-  },
-  images: {
-    domains: [getWpHostname()],
-  },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
   async rewrites() {
-    return [
-       {
-         source: "/sample-page/",
-         destination: "https://brandonteinert.wpengine.com/sample-page/",
-       },
-       {
-         source: "/ducks",
-         destination: "https://wpengine.com/wordpress-hosting",
-       },
-       {
-         source: "/tpp-bpatlas/",
-         destination: "https://bpatlasbptesti.wpengine.com/testing-proxy-pass/",
-       },
-       {
-         source: "/testing-proxy-pass",
-         destination: "http://bpatlasbptesti.wpengine.com/testing-proxy-pass",
-       },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/sample-page",
+          destination: "http://brandonteinert.com/sample-page/",
+        },
+      ],
+    };
   },
 });
 
