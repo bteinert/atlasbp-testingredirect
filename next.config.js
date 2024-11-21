555590712a97053323dc276c8b0d9ba92ca0306b
Brandon Teinert <10935840+bteinert@users.noreply.github.com>
2024-11-21 13:16:15 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ module.exports = withFaust({
       beforeFiles: [
         {
           source: "/sample-page",
-          destination: "https://gobreck.com/sample-page",
+          destination: "http://bpatlasbptesti.wpenginepowered.com/sample-page",
         },
         {
           source: "/sample-page/",
-          destination: "https://gobreck.com/sample-page/",
+          destination: "http://bpatlasbptesti.wpenginepowered.com/sample-page/",
         },
       ],
     };
